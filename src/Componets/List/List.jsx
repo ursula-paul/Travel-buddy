2c76a82cd8c9d3545bd6437919a202c8fca9a0a6
ursula-paul <ursula.paul@decagon.dev>
2022-03-01 17:19:51 +0100

    started a TravelBuddyProject

---
+++ src/Componets/List/List.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, createRef } from "react";
+
+import {
+	CircularProgress,
+	Grid,
+	Typography,
+	InputLabel,
+	MenuItem,
+	FormControl,
+	Select,
+} from "@material-ui/core";
+
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
+import useStyles from "./styles.js";
+
+const List = () => {
+	return (
+		<div>
+			<h1>List</h1>
+		</div>
+	);
+};
+
+export default List;
